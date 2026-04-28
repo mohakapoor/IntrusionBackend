0a4ae8da652657c8a4da457d4e233aa69eb371f3
mohakapoor <mohak.kapoor.v@gmail.com>
2026-04-28 06:51:19 +0000

    updated utils function by cleaning some comments

--- utils.py
+++ utils.py
@@ -8,31 +8,26 @@ from dotenv import load_dotenv
 warnings.filterwarnings("ignore")
 load_dotenv()
 
-# Pre-load models for efficiency
+# Pre-load scalars 
 MINMAX = joblib.load(os.getenv('PATH_MINMAX'))
 STANDARD = joblib.load(os.getenv('PATH_STANDARD'))
 PCA = joblib.load(os.getenv('PATH_PCA'))
 
-# Load dataset once into memory using Polars and add a row index
+
+# loading data
 df = pl.read_parquet("unscaled_test.parquet").with_row_index("original_index")
-# Separate features, target, and index
 y_test = df["Attack"]
 indices = df["original_index"]
 x = df.drop(["Attack", "original_index"])
 
 def sampler(target_class):
-    """
-    Samples one row and its original index using Polars.
-    """
-    # Filter for the target class and sample 1 row
-    # We filter the whole df to keep the relationship between x and original_index
+
     sample_df = df.filter(pl.col("Attack") == target_class).sample(1)
     
     if sample_df.height == 0:
         raise ValueError(f"No data found for class {target_class}")
     
     idx = sample_df["original_index"][0]
-    # Drop Attack and index columns for the model input
     features = sample_df.drop(["Attack", "original_index"]).to_numpy()
     
     return features, idx
